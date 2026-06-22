15896b5b5e69fd00a83ae8ebec5e71a8cf4190fe
Preetha1408-T <preetha007.titanobovapvtltd@gmail.com>
2026-06-22 13:13:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ dotenv.config();
 import mongoose from "mongoose";
 import app from "./app.js";
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 const DB = process.env.MONGODB_URI;
 
